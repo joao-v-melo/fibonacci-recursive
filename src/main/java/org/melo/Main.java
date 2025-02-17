856645437fb56e28eb4026b893acb8841d60a49c
Melo <joaovmelo.17@gmail.com>
2025-02-17 16:13:05 -0300

    Estrutura recursiva aplicada.

--- src/main/java/org/melo/Main.java
+++ src/main/java/org/melo/Main.java
@@ -1,7 +1,17 @@
 package org.melo;
 
 public class Main {
+    public static int fibonacci(int n){
+        if (n <= 1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n - 2);
+    }
+
     public static void main(String[] args) {
-        
+        int limit = 10;
+        for (int i =0; i < limit; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
     }
 }
